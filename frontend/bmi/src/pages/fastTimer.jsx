8755da6f8b4ba021b5044c5583e3239813e193dc
CLEMON EZEH <clemonezeh@gmail.com>
2023-03-02 13:24:42 +0100

    fast timer update

--- frontend/bmi/src/pages/fastTimer.jsx
+++ frontend/bmi/src/pages/fastTimer.jsx
@@ -14,61 +14,61 @@ const FastTimer = () => {
     <Box padding={10} height={"100vh"}>
       <Tabs variant='soft-rounded' colorScheme='blue'>
         <TabList>
-          <Tab>1hr</Tab>
-          <Tab>2hr</Tab>
-          <Tab>4hr</Tab>
-          <Tab>6hr</Tab>
-          <Tab>8hr</Tab>
           <Tab>12hr</Tab>
+          <Tab>14hr</Tab>
+          <Tab>16hr</Tab>
+          <Tab>18hr</Tab>
+          <Tab>20hr</Tab>
+          <Tab>22hr</Tab>
         </TabList>
         <TabPanels>
           <TabPanel>
             <Text fontSize={17} fontWeight={500}>
               {" "}
-              1hr Fast timer
-            </Text>
-            <Box style={{ textAlign: "center" }}>
-              <Countdown hours={1} minutes={0} seconds={0} />
-            </Box>
-          </TabPanel>
-          <TabPanel>
-            <Text fontSize={17} fontWeight={500}>
-              2hr Fast timer
-            </Text>
-            <Box style={{ textAlign: "center" }}>
-              <Countdown hours={2} minutes={0} seconds={0} />
-            </Box>
-          </TabPanel>
-          <TabPanel>
-            <Text fontSize={17} fontWeight={500}>
-              4hr Fast timer
-            </Text>
-            <Box style={{ textAlign: "center" }}>
-              <Countdown hours={4} minutes={0} seconds={0} />
-            </Box>
-          </TabPanel>
-          <TabPanel>
-            <Text fontSize={17} fontWeight={500}>
-              6hr Fast timer
-            </Text>
-            <Box style={{ textAlign: "center" }}>
-              <Countdown hours={6} minutes={0} seconds={0} />
-            </Box>
-          </TabPanel>
-          <TabPanel>
-            <Text fontSize={17} fontWeight={500}>
-              8hr Fast timer
-            </Text>
-            <Box style={{ textAlign: "center" }}>
-              <Countdown hours={8} minutes={0} seconds={0} />
-            </Box>
-          </TabPanel>
-          <TabPanel>
-            <Text fontSize={17} fontWeight={500}>
               12hr Fast timer
             </Text>
             <Box style={{ textAlign: "center" }}>
-              <Countdown hours={12} minutes={0} seconds={0} />
+              <Countdown hours={0} minutes={0} seconds={10} />
+            </Box>
+          </TabPanel>
+          <TabPanel>
+            <Text fontSize={17} fontWeight={500}>
+              14hr Fast timer
+            </Text>
+            <Box style={{ textAlign: "center" }}>
+              <Countdown hours={14} minutes={0} seconds={0} />
+            </Box>
+          </TabPanel>
+          <TabPanel>
+            <Text fontSize={17} fontWeight={500}>
+              16hr Fast timer
+            </Text>
+            <Box style={{ textAlign: "center" }}>
+              <Countdown hours={16} minutes={0} seconds={0} />
+            </Box>
+          </TabPanel>
+          <TabPanel>
+            <Text fontSize={17} fontWeight={500}>
+              18hr Fast timer
+            </Text>
+            <Box style={{ textAlign: "center" }}>
+              <Countdown hours={18} minutes={0} seconds={0} />
+            </Box>
+          </TabPanel>
+          <TabPanel>
+            <Text fontSize={17} fontWeight={500}>
+              20hr Fast timer
+            </Text>
+            <Box style={{ textAlign: "center" }}>
+              <Countdown hours={20} minutes={0} seconds={0} />
+            </Box>
+          </TabPanel>
+          <TabPanel>
+            <Text fontSize={17} fontWeight={500}>
+              22hr Fast timer
+            </Text>
+            <Box style={{ textAlign: "center" }}>
+              <Countdown hours={22} minutes={0} seconds={0} />
             </Box>
           </TabPanel>
         </TabPanels>
